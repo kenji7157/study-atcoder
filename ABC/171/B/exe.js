bbe715c96ee1bca9100c6e6563af293524bdd4a8
kenji7157 <kawake.kk@gmail.com>
2021-01-26 00:16:35 +0900

    171

---
+++ ABC/171/B/exe.js
@@ -0,0 +1,19 @@
+// inputに入力データ全体が入る
+function Main(input) {
+  input = input.split("\n");
+  let [n, k] = input[0].split(" ").map((x) => parseInt(x.trim()));
+  let pList = input[1]
+    .split(" ")
+    .map((x) => parseInt(x.trim()))
+    .sort((a, b) => {
+      return a < b ? -1 : 1;
+    });
+
+  console.log(
+    pList.slice(0, k).reduce((a, b) => {
+      return a + b;
+    })
+  );
+}
+//*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
+Main(require("fs").readFileSync("./dev/stdin", "utf8"));
